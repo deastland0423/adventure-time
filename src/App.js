9b2b1d40f7a17320b048f65d553fd9a28664078d
Daniel Eastland <daniel.eastland@gmail.com>
2021-02-03 17:56:55 -0600

    Removed unused files

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import List from './components/List';
-import withListLoading from './components/withListLoading';
-import axios from 'axios';
 import Roles from './components/Roles';
 import Users from './components/Users';
 
 function App() {
-  // const ListLoading = withListLoading(List);
-  // const [appState, setAppState] = useState({
-  //   loading: false,
-  //   repos: null,
-  // });
 
-  // useEffect(() => {
-  //   setAppState({
-  //     loading: true 
-  //   });
-    
-  //   const apiUrl = `https://xlxp3abvkg.execute-api.us-east-1.amazonaws.com/dev/users`;
-
-  //   axios.get(apiUrl).then( (repos) => {
-  //     const allRepos = repos.data;
-  //     console.log(allRepos);
-  //     setAppState( { loading: false, repos: allRepos});
-  //   });
-
-  //   // fetch(apiUrl)
-  //   //   .then((res) => res.json())
-  //   //   .then((repos) => {
-  //   //     setAppState({ loading: false, repos: repos });
-  //   //   });
-  // }, [setAppState]);
-  
   return (
     <div className='App'>
       <h1>Into The Unknown: Online Interface</h1>
-      {/* <div className='container'>
-        <h1>Into The Unknown: React App</h1>
-      </div>
-      <div className='repo-container'>
-        <ListLoading isLoading={appState.loading} repos={appState.repos} />
-      </div> */}
       <div>
         <h3>Registered Players</h3>
         <Users />
