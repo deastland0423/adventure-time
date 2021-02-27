e609e53521641fbfe511605e5179291b9f3d492d
Benjamin Bradley <benjamin@wegeekout.com>
2021-02-26 21:20:16 -0600

    enable localdev to override configuration

--- src/config.js
+++ src/config.js
@@ -1,5 +1,12 @@
-const AppConfig = {
+let localConfig = {};
+try {
+    localConfig = require('./config.local');
+} catch (err) { /*ignore*/ }
+const defaultAppConfig = {
   backend_host: "https://xlxp3abvkg.execute-api.us-east-1.amazonaws.com/dev"
-  //backend_host: "http://localhost:4001"
 };
+const AppConfig = {
+    ...defaultAppConfig,
+    ...localConfig
+}
 export default AppConfig;
