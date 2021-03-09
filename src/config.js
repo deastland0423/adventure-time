7173f4c09dd5c5148051e8b4e94824baa2afb78c
Benjamin Bradley <benjamin@wegeekout.com>
2021-03-09 16:00:07 -0600

    add empty config.local.js so build won't break, keep it in .gitignore so local customizations will be ignored

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
-let localConfig = {};
-// try {
-//     localConfig = require('./config.local');
-// } catch (err) { /*ignore*/ }
+const localConfig = require('./config.local');
 const defaultAppConfig = {
   backend_host: "https://62zrxtutca.execute-api.us-east-1.amazonaws.com/dev"
 };
---
+++ src/config.local.js
@@ -0,0 +1,5 @@
+// To customize your local configuration, copy this file to config.local.js and set values as desired to override those in config.js
+const localAppConfig = {
+//  backend_host: "http://localhost:4001"
+};
+module.exports = localAppConfig;
