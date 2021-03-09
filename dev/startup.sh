2baf9b19ffd87b66efef4ff9dfd690cf41211599
Benjamin Bradley <benjamin@wegeekout.com>
2021-03-09 15:56:49 -0600

    switch to npm for localdev

--- dev/startup.sh
+++ dev/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e  # halt on error
 set -x  # show commands
-yarn install # install dependencies
-yarn start  # startup app
+npm install # install dependencies
+npm start  # startup app
