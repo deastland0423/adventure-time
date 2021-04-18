8f0b0b1e7b0f1d04dafe325069c66e7119ed8ec5
Benjamin Bradley <benjamin@wegeekout.com>
2021-04-18 16:55:54 -0500

    add hex background image

--- src/components/map/HexMap.js
+++ src/components/map/HexMap.js
@@ -3,26 +3,37 @@ import { useModalContext, showModal } from "../../contexts/ModalContext";
 import { hexCoordsDisplay } from '../../utils/formattingUtils';
 import './map.css';
 import HexDetail from './HexDetail';
+import terrain_forest from '../../images/terrain_forest.png';
 
 const HexMap = () => {
     const { dispatch: dispatchModal } = useModalContext();
     const canvasRef = useRef(null);
+    // Hard-coded map constants
+    const mapview_height_hexes = 3; // How many full hexes are shown in the map view.
+    const grid_cols = 10;   // How wide the world map is in hexes.
+    const grid_rows = 10;   // How tall the world map is in hexes.
+    // User-modifiable map "settings"
     const settings_show_hex_coordinates = true;
-    const settings_hex_radius = 100;
-    const settings_grid_cols = 9;
-    const settings_grid_rows = 3;
+    const settings_hex_radius = 368/2;
+    const settings_initial_topleft_grid_x = 2;
+    const settings_initial_topleft_grid_y = 6;
+    // calculate derived constants
+    const hex_height = Math.ceil(settings_hex_radius * Math.sin(2*Math.PI * 1/6)) * 2;
+    const hex_width = settings_hex_radius * 2;
+    const map_height = mapview_height_hexes * hex_height;
+    let mapview_base_x = -settings_initial_topleft_grid_x * hex_width;
+    let mapview_base_y = -settings_initial_topleft_grid_y * hex_height;
 
     useEffect(() => {
+        // draw the hexagons
         const hex_radius = settings_hex_radius;
-        const hex_height = Math.ceil(hex_radius * Math.sin(2*Math.PI * 1/6)) * 2;
-        const hex_width = Math.ceil(hex_radius * Math.cos(2*Math.PI * 0/6)) * 2;
         const hex_spacing = Math.ceil(  hex_radius * (Math.cos(2*Math.PI * 1/6) - Math.cos(2*Math.PI * 2/6) )   );
-        for (let grid_x=0; grid_x<settings_grid_cols; grid_x++) {
-            for (let grid_y=0; grid_y<settings_grid_rows; grid_y++) {
+        for (let grid_x=0; grid_x<grid_cols; grid_x++) {
+            for (let grid_y=0; grid_y<grid_rows; grid_y++) {
                 // calculate pixel coordinates for hex from grid coordinates
                 const angle_width = (hex_width-hex_spacing)/2;
-                let base_x = Math.ceil(hex_radius + grid_x*(hex_spacing+angle_width));
-                let base_y = Math.ceil(grid_y*hex_height + hex_height/2);
+                let base_x = mapview_base_x + Math.ceil(hex_radius + grid_x*(hex_spacing+angle_width));
+                let base_y = mapview_base_y + Math.ceil(grid_y*hex_height + hex_height/2);
                 if (grid_x % 2 === 1) {  // offset for odd rows
                     base_y += Math.ceil(hex_height/2);
                 }
@@ -41,6 +52,9 @@ const HexMap = () => {
                 hex.setAttribute("points", poly_def);
                 hex.setAttribute("data-coords",`${grid_x},${grid_y}`);
                 hex.setAttribute("class","hex");
+                if("terrain_type === forest") {
+                    hex.setAttribute("fill", "url(#forest_bg)")
+                }
                 canvasRef.current.appendChild(hex);
                 hex.addEventListener("click", (event) => {
                     const hexCoords = event.target.getAttribute('data-coords');
@@ -64,7 +78,12 @@ const HexMap = () => {
 
     return (
         <div id="hexmap">
-            <svg xmlns="http://www.w3.org/2000/svg" ref={canvasRef}>
+            <svg xmlns="http://www.w3.org/2000/svg" ref={canvasRef} height={map_height} width="100%">
+                <defs>
+                    <pattern id="forest_bg" height="100%" width="100%">
+                        <image x="0" y="0" height={hex_height} width={hex_width} xlinkHref={terrain_forest}></image>
+                    </pattern>
+                </defs>
             </svg>
         </div>
     );
